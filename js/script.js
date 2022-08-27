247b38784f0c4bfc57fd61bb710fd8d6d1c82c58
CodingTheWall <oualid.m.chelgoum@gmail.com>
2022-08-28 01:31:59 +0200

    My first commit update

---
+++ js/script.js
@@ -0,0 +1,39 @@
+const form = document.getElementById('generate-form')
+const qr = document.getElementById('qrcode')
+
+
+const onGenerateSubmit = (e) => {
+    e.preventDefault();
+    
+    const url = document.getElementById('url').value;
+    const size = document.getElementById('size').value;
+
+    if(url ==='') {
+        alert('Please enter a url')
+    }else{
+        showSpinner();
+
+        setTimeout(() =>{
+        hideSpinner();
+        } ,1000)
+    }
+};
+
+const generateQRcode = (url,size) => {
+    const qrcode = newQRCode ('qrcode',{
+        text: url,
+        width: size,
+        height: size,
+
+    })
+}
+
+const showSpinner = () => {
+    document.getElementById('spinner').style.display = 'block'
+};
+const hideSpinner = () => {
+    document.getElementById('spinner').style.display = 'none'
+};
+
+
+form.addEventListener('submit',onGenerateSubmit)
